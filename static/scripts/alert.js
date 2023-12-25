b4fd4116230c4d6701e7506f62001354fc59f49b
Mark Hillman <mark@markhillman.info>
2023-12-25 22:05:16 +0000

    Code cleanup and comments

--- static/scripts/alert.js
+++ static/scripts/alert.js
@@ -6,8 +6,10 @@ const alertClass = 'alert'
 const successClass = 'success';
 const errorClass = 'error'
 
-// Initialize the alert container. Returns a promise that resolves when the
-// container has been initialized for alerts.
+/**
+ * Initializes the alert functionality.
+ * @returns {Promise<void>} A promise that resolves when the initialization is complete.
+ */
 export function init() {
     return new Promise((resolve, reject) => {
         alertContainer = document.getElementById('alert-container');
@@ -15,18 +17,35 @@ export function init() {
     });
 }
 
+/**
+ * Creates an alert with the given content.
+ * @param {string} content - The content of the alert.
+ */
 export function alert(content) {
     createAlert(content);
 }
 
+/**
+ * Displays a success alert with the given content.
+ * @param {string} content - The content to be displayed in the success alert.
+ */
 export function success(content) {
     createAlert("Success: " + content, successClass);
 }
 
+/**
+ * Displays an error alert with the given content.
+ * @param {string} content - The content of the error alert.
+ */
 export function error(content) {
     createAlert("Error: " + content, errorClass);
 }
 
+/**
+ * Creates an alert with the given content and optional alert classes.
+ * @param {string} content - The content of the alert.
+ * @param {...string} alertClasses - Optional classes to be applied to the alert.
+ */
 function createAlert(content, ...alertClasses) {
     var alertElement = generateAlertElement('section', content, alertClasses);
     if (alertContainer != null) {
@@ -47,6 +66,13 @@ function createAlert(content, ...alertClasses) {
     setTimeout(() => closeAlert(alertElement), 5 * 1000);
 }
 
+/**
+ * Generates an alert element with the specified tag, content, and alert classes.
+ * @param {string} tag - The HTML tag for the alert element.
+ * @param {string} content - The content to be displayed in the alert element.
+ * @param {...string} alertClasses - Additional classes to be added to the alert element.
+ * @returns {HTMLElement} - The generated alert element.
+ */
 function generateAlertElement(tag, content, ...alertClasses) {
     var alertElement = document.createElement(tag);
     alertElement.className = alertClass + " " + alertClasses.join(" ");
@@ -64,6 +90,10 @@ function generateAlertElement(tag, content, ...alertClasses) {
     return alertElement;
 }
 
+/**
+ * Closes the alert element.
+ * @param {HTMLElement} alertElement - The alert element to be closed.
+ */
 function closeAlert(alertElement) {
     var tl3 = gsap.timeline({delay: 0});
     tl3.to(alertElement, {
